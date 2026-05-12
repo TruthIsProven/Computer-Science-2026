e8820b2e400b35268233297094c838e69e2d414a
TruthIsProven <eve.morre.021031@gmail.com>
2026-05-13 10:26:18 +1200

    Buttons work

--- starting_screen.gd
+++ starting_screen.gd
@@ -1,2 +1,13 @@
 extends Control
 # Comment comment
+
+
+func _on_new_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://newgame_scene.tscn")
+	pass # Replace with function body.
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
