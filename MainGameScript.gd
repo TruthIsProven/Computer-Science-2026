98dff744e6a7fc51875f1fc66795eefead865d1e
TruthIsProven <eve.morre.021031@gmail.com>
2026-08-20 14:28:16 +1200

    Editing Json

--- MainGameScript.gd
+++ MainGameScript.gd
@@ -10,14 +10,14 @@ const WIKI_FILE_PATH := "res://entries/entries.json"
 var wiki_articles: Dictionary = {}
 
 
-func _ready() -> void:
-	load_wiki_articles()
+func _ready() -> void: #Function that calls the designated article in the JSON file through the article number labelled with the button
+	load_wiki_articles() #call the loading article/wiki file function
 
-	if wiki_articles.has("article_1"):
+	if wiki_articles.has("article_1"): #starting from article one and match the same numbered article to the same title in the JSON file
 		open_article("article_1")
 
 
-func load_wiki_articles() -> void:
+func load_wiki_articles() -> void:#function that loads the designated articles and show error message when the JSON file is not found
 	if not FileAccess.file_exists(WIKI_FILE_PATH):
 		push_error("Wiki file was not found: " + WIKI_FILE_PATH)
 		rich_label.text = "The wiki database could not be found."
@@ -27,7 +27,7 @@ func load_wiki_articles() -> void:
 
 	if file == null:
 		push_error("Godot could not open the wiki file.")
-		rich_label.text = "The wiki database could not be opened."
+		rich_label.text = "The wiki database could not be opened." 
 		return
 
 	var json_text := file.get_as_text()
@@ -59,9 +59,9 @@ func load_wiki_articles() -> void:
 	print("Loaded ", wiki_articles.size(), " wiki articles.")
 
 
-func open_article(article_id: String) -> void:
-	if not wiki_articles.has(article_id):
-		push_warning("Unknown wiki article: " + article_id)
+func open_article(article_id: String) -> void: #Get the different part of the article and show error message when not found
+	if not wiki_articles.has(article_id): #Error message function for if there are no article found under the name/number of article
+		push_warning("Unknown wiki article: " + article_id) 
 		rich_label.text = "ARTICLE NOT FOUND"
 		return
 
@@ -78,20 +78,22 @@ func open_article(article_id: String) -> void:
 		"No recommended action is available."
 	)
 
-	var page_text := ""
+	var page_text := "" #Stylize the fonts (size, font) and formatting on a wiki page
 
 	page_text += "[font_size=40][b]"
 	page_text += title
 	page_text += "[/b][/font_size]\n"
 
 	page_text += "\n[font_size=30][b]Description[/b][/font_size]\n"
+	page_text += "[font_size=18]"
 	page_text += description
+	page_text += "[/font_size]"
 	page_text += "\n\n"
 
 	page_text += "[font_size=30][b]Danger[/b][/font_size]\n"
 
 	for sign_text in danger:
-		page_text += "[font_size=20]"
+		page_text += "[font_size=18]"
 		page_text += str(sign_text) + "\n"
 		page_text += "[/font_size]"
 
@@ -139,3 +141,8 @@ func _on_button_9_pressed() -> void:
 
 func _on_button_10_pressed() -> void:
 	open_article("article_10")
+
+
+func _on_exitbutton_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/newgame_scene.tscn")
+	pass # Replace with function body.
